7e31fc75e01163db98494d12072f426c7e368796
alejo190404 <alejo190404@gmail.com>
2024-04-30 08:35:40 -0500

    Datos de administrador agregados

--- demo/src/main/java/com/vetlove/demo/Entidad/DatabaseInit.java
+++ demo/src/main/java/com/vetlove/demo/Entidad/DatabaseInit.java
@@ -13,6 +13,7 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Controller;
 
+import com.vetlove.demo.Repositorio.AdministradorRepositorio;
 import com.vetlove.demo.Repositorio.ClienteRepositorio;
 import com.vetlove.demo.Repositorio.EnfermedadRepositorio;
 import com.vetlove.demo.Repositorio.EspecialidadRepositorio;
@@ -59,6 +60,9 @@ public class DatabaseInit implements ApplicationRunner {
 
         @Autowired
         EstadoVetRepositorio repoEstadoVet;
+
+        @Autowired
+        AdministradorRepositorio repoAdministrador;
 
         @Override
         public void run(ApplicationArguments args) throws Exception {
@@ -608,7 +612,10 @@ public class DatabaseInit implements ApplicationRunner {
                         }
                         repoTratamiento.save(tratamiento);
                 }
-
+                
+                Administrador admin = new Administrador("1000586123", "super-secret-password");
+                repoAdministrador.save(admin);
         }
+
 
 }
