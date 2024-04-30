52650096acc4b92c9b27faa7fdbd1286117a002a
alejo190404 <alejo190404@gmail.com>
2024-04-30 08:28:53 -0500

    Capas de EstadoVet finalizadas. Solo un método para retornar todos los estados

--- demo/src/main/java/com/vetlove/demo/Interfaz/IEstadoVetServicio.java
+++ demo/src/main/java/com/vetlove/demo/Interfaz/IEstadoVetServicio.java
@@ -1,8 +1,11 @@
 package com.vetlove.demo.Interfaz;
 
+import java.util.List;
+
 import com.vetlove.demo.Entidad.EstadoVet;
 
 public interface IEstadoVetServicio {
     public EstadoVet SearchById(Long id);
     public EstadoVet SearchByName(String name);
+    public List<EstadoVet> SearchAll();
 }
