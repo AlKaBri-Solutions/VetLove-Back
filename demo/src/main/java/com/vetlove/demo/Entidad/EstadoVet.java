f923c54ac2de99c375660aa89b73c9449e65bb03
J-Brinez <juanf.brinez@gmail.com>
2024-05-21 16:26:53 -0500

    Errores pequeños corregidos

--- demo/src/main/java/com/vetlove/demo/Entidad/EstadoVet.java
+++ demo/src/main/java/com/vetlove/demo/Entidad/EstadoVet.java
@@ -9,12 +9,15 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Data
-@NoArgsConstructor
+@Builder
+@AllArgsConstructor
 public class EstadoVet {
     @Id
     @GeneratedValue
